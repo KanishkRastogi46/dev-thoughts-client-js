f64c808664ba494f2d8a04900dfa641d790e0316
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-08-31 20:25:29 +0530

    initial commit with react-router and tailwind

---
+++ app/routes.ts
@@ -0,0 +1,12 @@
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx"),
+
+    route("auth", "routes/auth/layout.tsx", [
+        route("login", "routes/auth/login/page.tsx"),
+        route("register", "routes/auth/register/page.tsx"),
+        route("otp", "routes/auth/otp/page.tsx"),
+        route("forgot-password", "routes/auth/forgot-password/page.tsx"),
+    ])
+] satisfies RouteConfig;
